36f0def3da6e988b784ff69070784392ac4874b6
Alexis <Alexismahuad2052@gmail.com>
2021-07-09 12:31:47 -0300

    Bug fixes

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/i,
+        use: ["file-loader?name=assets/[name].[ext]", "image-webpack-loader"],
+      },
     ],
   },
   devtool: "inline-source-map",
